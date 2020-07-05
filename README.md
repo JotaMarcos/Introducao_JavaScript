4769f2ac1be7381bb5c94255f495f18f4d8a7c39
JotaMarcos <jmarcos_duarte@hotmail.com>
2020-07-05 02:04:10 -0400

    Criação do index.html e do main.js.

---
+++ README.md
@@ -0,0 +1,2 @@
+#  Introducao_JavaScript
+Introdução a Programação para Internet com o JavaScript.
---
+++ js/main.js
@@ -0,0 +1,163 @@
+
+
+
+function clicou(){
+    //alert("Obrigado por Clicar!!!")
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar!!! Clique Aqui!!</b>";
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar(){
+    window.open("http://www.masterbare.com.br/");
+    //window.location.href = "http://www.masterbare.com.br/";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "<b>Obrigado por Passar o Mouse!!!</b>";
+    // document.getElementById("mousemove").innerHTML = "<b>Obrigado por Passar o Mouse!!!</b>";
+    //alert("Trocar o Texto!!!");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o Mouse Aqui!!!";
+    // document.getElementById("mousemove").innerHTML = "Passe o Mouse Aqui!!!";
+}
+
+function load(){
+    alert("Página Carregada!!!");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// function setReplace(frase, nome, novo_nome){
+//     return frase.replace(nome, novo_nome);
+// }
+
+// function validaIdade(idade){
+//     var validar;
+//     if(idade >= 18){
+//         validar = true;
+//     } else {
+//         validar = false;
+//     }
+//     return validar;
+// }
+
+
+// var validar = 0;
+// function validaIdade(idade){
+//     if(idade >= 18){
+//         validar = true;
+//     } else {
+//         validar = false;
+//     }
+//     return validar;
+// }
+
+
+// var idade = prompt("Qual a sua Idade?: ");
+// validaIdade(idade);
+// alert(validar);
+// validaIdade(idade);
+// console.log(validar);
+
+// alert(soma(5, 10));
+// console.log(soma(5, 10));
+// alert(setReplace("Vai Japão", "Japão", "Brasil"));
+// console.log(setReplace("Vai Japão", "Japão", "Brasil"));
+
+/*var d = new Date();
+console.log(d);
+alert(d);
+console.log(d.getMonth() + 1);
+alert(d.getMonth() + 1);
+console.log(d.getDay());
+alert(d.getDay());
+console.log(d.getHours());
+alert(d.getHours());
+console.log(d.getMinutes());
+alert(d.getMinutes());
+console.log(d.getSeconds());
+alert(d.getSeconds());*/
+
+
+/*for(var count = 0; count <= 5; count++){
+    console.log(count);
+    alert(count);
+}*/
+
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count)
+    count++; //count = count + 1; ou count += 1;
+};*/
+// var idade = prompt("Qual a sua Idade?: ");
+// var idade = 18;
+// if(idade >= 18){
+//     alert("Maior de Idade!!!");
+// } else {
+//     alert("Menor de Idade!!!");
+// };
+
+
+
+// var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+
+// var fruta = {nome: "maçã", cor: "vermelha"}
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var  lista = ["maçã", "pêra", "laranja"];
+// lista.push("uva");
+//lista.pop();
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+//console.log(lista[1]);
+//alert(lista[1]);
+
+
+// var nome = "João Marcos Aires Duarte"
+// var idade = 35;
+// alert("Olá, Mundo do Desenvolvimento Web com JavaScript!!!\\o/\\o/\\o/");
+// alert("Bem Vindo " + nome + "!!!\\o/\\o/\\o/");
+// alert(nome + " tem " + idade + " anos!!!");
+// var idade = 35;
+// var idade2 = 10;
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Japão é o melhor time do mundo!!!";
+// alert(idade + idade2 + " anos!!!"); 
+// console.log(nome);
+// console.log(n1 + n2);
+// console.log(n1 - n2);
+// console.log(n1 * n2);
+// console.log(n1 / n2);
+// console.log(n1 % n2);
+//console.log(frase.replace("Japão", "Brasil")
+// console.log(frase.replace("Japão", "Brasil").toUpperCase());
+// console.log(frase.toUpperCase());
+// console.log(frase.replace("Japão", "Brasil").toLowerCase());
+// console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+
+
+
+
+
+
+
+
